eca176b460edaab8f8608b1d0bda13652bb4c1d6
edwinacubillos <edwinacubillos@gmail.com>
2022-07-26 11:08:49 -0500

    Enviando a Github

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,88 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app" class="app">
+
+    <div class="header">
+
+      <h1>Banco Mision TIC</h1>
+      <nav>
+        <button v-if = "is_auth">Inicio</button>
+        <button v-if = "is_auth">Cuenta</button>
+        <button v-if = "is_auth">Cerrar Sesión</button>
+        <button v-if = "!is_auth" v-on:click = "loadLogIn">Iniciar Sesión</button>
+        <button v-if = "!is_auth" v-on:click = "loadSignUp">Registrarse</button>
+      </nav>
+    </div>
+
+    <div class="main-component">
+      <router-view
+        v-on:completedLogIn = "completedLogIn"
+        v-on:completedSignUp = "completedSignUp">
+      </router-view>
+    </div>
+
+    <div class="footer">
+      <h2>Misión TIC 2022</h2>
+    </div>
+
+  </div>
 </template>
 
+<script>
+export default{
+  name:'App',
+
+  data: function(){
+    return{
+      is_auth: false
+    }
+  },
+
+  methods:{
+    verifyAuth: function(){
+      if (this.is_auth == false)
+        this$router.push({name: "logIn"})
+    },
+
+    loadLogin: function(){
+      this.$router.push({name: "logIn"})
+    },
+
+    loadSignUp: function(){
+      this.$router.push({name: "signUp"})
+    },
+
+    completedLogIn: function(data) {},
+
+    completedSignUp: function(data) {},
+  },
+
+  created: function(){
+    this.verifyAuth()
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  body{
+    margin: 0 0 0 0;
+  }
 
-nav {
-  padding: 30px;
-}
+  .header{
+    margin:0;
+    padding:0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    background-color: #283747;
+    color: #E5E7E9;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    .header h1{
+      width: 20%;
+      text-align: center;
+    }
 </style>
